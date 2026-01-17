9f8c6d6a59c05299c5b8c3f89986d09d1c10c937
Huynh Duc Dung <dunghd.it@gmail.com>
2026-01-17 23:48:53 +0800

    chore(release): v0.1.1

--- bump.config.ts
+++ bump.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "bumpp";
 
 export default defineConfig({
   files: ["package.json"],
-  commit: "chore(release): v{newVersion}",
-  tag: "v{newVersion}",
+  commit: "chore(release): v{version}",
+  tag: "v{version}",
   push: true,
   confirm: true,
 });
